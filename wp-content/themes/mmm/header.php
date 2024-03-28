1c5b834997f76e79929c1fb577d3760b8c74e101
Ben Reiner <ben@mcguinnessmedia.com>
2024-03-28 11:19:53 -0400

    Add site titles

--- wp-content/themes/mmm/header.php
+++ wp-content/themes/mmm/header.php
@@ -3,7 +3,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?php get_the_title() ?> | <?php bloginfo('name') ?></title>
+  <title>
+    <?php if(is_front_page()): ?>
+    <?php bloginfo('name') ?>
+    <?php echo get_bloginfo('description') == '' ? '' : ' | ' . get_bloginfo('description'); ?>
+    <?php else: ?>
+    <?php get_the_title() ?> | <?php bloginfo('name') ?>
+    <?php endif; ?>
+  </title>
 </head>
 <body>
   <header>
