5a3f2de3a4c09b15d0b32cdd9c96fef7dde050bf
Ben Reiner <ben@mcguinnessmedia.com>
2024-04-23 17:02:12 -0400

    Add recipient custom post type

---
+++ wp-content/mu-plugins/post-types.php
@@ -0,0 +1,44 @@
+<?php
+
+// Register custom post types
+add_action('init', 'slgRegisterPostTypes');
+function slgRegisterPostTypes()
+{
+  // Scholarship recipients
+  register_post_type(
+    'recipient',
+    array(
+      'description' => 'Scholarship recipients',
+      'public' => true,
+      'labels' => array(
+        'name' => __('Recipients', 'example-textdomain'),
+        'singular_name' => __('Recipient', 'example-textdomain'),
+        'menu_name' => __('Recipients', 'example-textdomain'),
+        'name_admin_bar' => __('Recipientss', 'example-textdomain'),
+        'add_new' => __('Add New', 'example-textdomain'),
+        'add_new_item' => __('Add New Recipient', 'example-textdomain'),
+        'edit_item' => __('Edit Recipient', 'example-textdomain'),
+        'new_item' => __('New Recipient', 'example-textdomain'),
+        'view_item' => __('View Recipient', 'example-textdomain'),
+        'search_items' => __('Search Recipients', 'example-textdomain'),
+        'not_found' => __('No recipients found', 'example-textdomain'),
+        'not_found_in_trash' => __('No recipients found in trash', 'example-textdomain'),
+        'all_items' => __('All Recipients', 'example-textdomain'),
+      ),
+      'supports' => array('title'),
+    )
+  );
+}
+
+add_filter('enter_title_here', 'slgChangeTitleText');
+function
+  slgChangeTitleText($title)
+{
+  $screen = get_current_screen();
+
+  if ($screen->post_type == 'recipient') {
+    $title = 'Recipient name';
+  }
+
+  return $title;
+}
