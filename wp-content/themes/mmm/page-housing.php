6ec020b6b69d6f9acc473b5a22d0e811fa5dc7d6
Ben Reiner <ben@mcguinnessmedia.com>
2024-04-24 14:33:03 -0400

    Add housing page

--- wp-content/themes/mmm/page-housing.php
+++ wp-content/themes/mmm/page-housing.php
@@ -1,5 +1,45 @@
-<?php get_header(); ?>
+<?php get_header('', array('pageId' => 'housing'));
+while (have_posts()):
+  the_post();
 
-<h1>Hello world!</h1>
+  $hero = get_field('hero');
+  if ($hero):
+    ?>
+    <section class="hero">
+      <img src="" alt="">
+      <div class="hero__content">
+        <h1><?php the_title() ?></h1>
+        <?php if ($hero['copy']): ?>
+          <p>
+            <?php echo esc_attr($hero['copy']) ?>
+          </p>
+        <?php endif;
+        if ($hero['button_url']): ?>
+          <a href="<?php echo esc_url($hero['button_url']) ?>" class="btn btn-primary">
+            <?php echo esc_attr($hero['button_label']) ?>
+          </a>
+        <?php endif; ?>
+      </div>
+    </section>
+    <?php
+  endif;
 
-<?php get_footer(); ?>
+  $cta = get_field('call_to_action');
+  if ($cta):
+    ?>
+    <section class="call-to-action">
+      <div class="call-to-action__content">
+        <h2><?php echo esc_attr($cta['title']) ?></h2>
+        <p><?php echo esc_attr($cta['copy']) ?></p>
+        <?php if ($cta['button_url']): ?>
+          <a href="<?php echo esc_url($cta['button_url']) ?>"
+            class="btn btn-outline"><?php echo esc_attr($cta['button_label']) ?>
+          </a>
+        <?php endif; ?>
+      </div>
+      <img src="" alt="">
+    </section>
+    <?php
+  endif;
+endwhile;
+get_footer(); ?>
