453f61475886331dd36a40a5f9ce3b849c8a6ef0
Ben Reiner <ben@mcguinnessmedia.com>
2024-05-02 14:44:38 -0400

    Add hero and CTA css files

--- wp-content/themes/mmm/functions.php
+++ wp-content/themes/mmm/functions.php
@@ -1,11 +1,28 @@
 <?php
 
-// Register swiper on scholarships page
-add_action('wp_enqueue_scripts', 'slgRegisterSwiper');
-function slgRegisterSwiper()
+// Enqueue styles & scripts
+add_action('wp_enqueue_scripts', 'slgEnqueueFiles');
+function slgEnqueueFiles()
 {
+  wp_enqueue_style('futura-fonts', 'https://use.typekit.net/hei8erc.css');
+
+  // Register Swiper only on scholarships page
   if (is_page('scholarships')) {
-    wp_enqueue_style('slg-swiper-css', get_template_directory_uri() . '/css/swiper.min.css');
-    wp_enqueue_script('slg-swiper-js', get_template_directory_uri() . '/js/swiper.min.js');
+    wp_enqueue_style('slg-swiper', get_template_directory_uri() . '/css/swiper.min.css');
+    wp_enqueue_script('slg-swiper', get_template_directory_uri() . '/js/swiper.min.js');
   }
+}
+
+// Enqueue hero CSS when template part is called
+add_action('get_template_part_hero', 'slgEnqueueHeroCSS');
+function slgEnqueueHeroCSS()
+{
+  wp_enqueue_style('slg-hero', get_template_directory_uri() . '/css/hero.css');
+}
+
+// Enqueue CTA CSS when template part is called
+add_action('get_template_part_call-to-action', 'slgEnqueueCtaCSS');
+function slgEnqueueCtaCSS()
+{
+  wp_enqueue_style('slg-call-to-action', get_template_directory_uri() . '/css/call-to-action.css');
 }
