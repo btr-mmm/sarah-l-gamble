f93f81903f749abefd442d5e11d2078ec11cf087
Ben Reiner <ben@mcguinnessmedia.com>
2024-05-28 16:05:36 -0400

    Add hero CSS

--- wp-content/themes/mmm/template-parts/hero.php
+++ wp-content/themes/mmm/template-parts/hero.php
@@ -1,16 +1,18 @@
 <?php $hero = get_field('hero'); ?>
 <section class="hero">
   <img src="" alt="">
-  <div class="hero__content">
-    <?php if ($hero['copy']): ?>
-      <p>
-        <?php echo esc_attr($hero['copy']) ?>
-      </p>
-    <?php endif;
-    if ($hero['button_url']): ?>
-      <a href="<?php echo esc_url($hero['button_url']) ?>" class="btn btn-primary">
-        <?php echo esc_attr($hero['button_label']) ?>
-      </a>
-    <?php endif; ?>
+  <div class="hero__content-container">
+    <div class="hero__content">
+      <?php if ($hero['copy']): ?>
+        <p>
+          <?php echo esc_attr($hero['copy']) ?>
+        </p>
+      <?php endif;
+      if ($hero['button_url']): ?>
+        <a href="<?php echo esc_url($hero['button_url']) ?>" class="btn btn-primary">
+          <?php echo esc_attr($hero['button_label']) ?>
+        </a>
+      <?php endif; ?>
+    </div>
   </div>
 </section>
