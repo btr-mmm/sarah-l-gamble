e27fa98fb824bc06c49b7c8617757678e7864578
Ben Reiner <ben@mcguinnessmedia.com>
2024-05-30 15:14:12 -0400

    Adjust font size

--- wp-content/themes/mmm/page-contact.php
+++ wp-content/themes/mmm/page-contact.php
@@ -6,7 +6,10 @@ while (have_posts()):
   get_template_part("template-parts/hero", '');
   ?>
 
-
+  <section class="contact-page">
+    <h2>Questions?</h2>
+    <p>Contact us at <span class="bold-text"><?php echo esc_attr(get_field('phone')) ?></span> or via email at <span< /p>
+  </section>
 
 <?php endwhile;
 get_footer(); ?>
