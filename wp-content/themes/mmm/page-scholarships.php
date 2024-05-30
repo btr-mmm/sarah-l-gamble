2618472b972e394c071c1e92892742a24501186f
Ben Reiner <ben@mcguinnessmedia.com>
2024-05-30 13:28:04 -0400

    Add recipients body

--- wp-content/themes/mmm/page-scholarships.php
+++ wp-content/themes/mmm/page-scholarships.php
@@ -1,5 +1,26 @@
-<?php get_header(); ?>
+<?php get_header();
 
-<h1>Hello world!</h1>
+while (have_posts()):
+  the_post();
+  ?>
+  <h1 class="sr-only"><?php the_title(); ?></h1>
+  <?php $recipientsSection = get_field('recipients');
+  if ($recipientsSection): ?>
+    <section class="recipients">
+      <div class="recipients-body">
+        <?php if (isset($recipientsSection['heading'])): ?>
+          <h2>
+            <?php echo esc_attr($recipientsSection['heading']) ?>
+          </h2>
+        <?php endif;
+        if (isset($recipientsSection['body'])): ?>
+          <p>
+            <?php echo esc_attr($recipientsSection['body']) ?>
+          </p>
+        <?php endif; ?>
+      </div>
+    </section>
+  <?php endif; ?>
 
-<?php get_footer(); ?>
+<?php endwhile;
+get_footer(); ?>
