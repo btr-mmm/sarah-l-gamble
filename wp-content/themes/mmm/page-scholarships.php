0fa016ebb05687ffe62512e6d83cabeb4b292448
Ben Reiner <ben@mcguinnessmedia.com>
2024-05-31 11:33:20 -0400

    Add slider styles

--- wp-content/themes/mmm/page-scholarships.php
+++ wp-content/themes/mmm/page-scholarships.php
@@ -7,71 +7,73 @@ while (have_posts()):
   <?php $recipientsSection = get_field('recipients');
   if ($recipientsSection): ?>
     <section class="recipients">
-      <div class="recipients-body">
-        <?php if (isset($recipientsSection['heading'])): ?>
-          <h2>
-            <?php echo esc_attr($recipientsSection['heading']) ?>
-          </h2>
-        <?php endif;
-        if (isset($recipientsSection['body'])): ?>
-          <p>
-            <?php echo esc_attr($recipientsSection['body']) ?>
-          </p>
+      <div class="container">
+        <div class="recipients-body">
+          <?php if (isset($recipientsSection['heading'])): ?>
+            <h2>
+              <?php echo esc_attr($recipientsSection['heading']) ?>
+            </h2>
+          <?php endif;
+          if (isset($recipientsSection['body'])): ?>
+            <p>
+              <?php echo esc_attr($recipientsSection['body']) ?>
+            </p>
+          <?php endif; ?>
+        </div>
+        <?php $recipientsQuery = new WP_Query(
+          array(
+            'post_type' => 'recipient',
+            'posts_per_page' => '-1',
+          )
+        );
+        if ($recipientsQuery->have_posts()):
+          ?>
+          <div class="swiper recipients-slider">
+            <div class="swiper-wrapper">
+              <?php while ($recipientsQuery->have_posts()):
+                $recipientsQuery->the_post(); ?>
+                <div class="swiper-slide">
+                  <?php if (get_field('photo')):
+                    $photo = get_field('photo') ?>
+                    <img src="<?php echo esc_url($photo['sizes']['recipients-slider']) ?>" alt="<?php the_title() ?>">
+                  <?php endif ?>
+                  <div class="slide-content">
+                    <h3><?php the_title(); ?></h3>
+                    <?php if (get_field('school')): ?>
+                      <p class="school">
+                        <?php echo esc_attr(get_field('school')) ?>
+                      </p>
+                    <?php endif;
+                    if (get_field('bio')): ?>
+                      <p>
+                        <?php echo esc_attr(get_field('bio')) ?>
+                      </p>
+                    <?php endif; ?>
+                  </div>
+                </div>
+              <?php endwhile;
+              wp_reset_postdata(); ?>
+            </div>
+            <div class="swiper-pagination"></div>
+            <button class="swiper-button-prev"></button><button class="swiper-button-next"></button>
+          </div>
+          <script>
+            const recipientsSlider = new Swiper(
+              '.swiper.recipients-slider',
+              {
+                loop: true,
+                navigation: {
+                  nextEl: '.recipients-slider .swiper-button-next',
+                  prevEl: '.recipients-slider .swiper-button-prev',
+                },
+                pagination: {
+                  el: '.recipients-slider .swiper-pagination'
+                }
+              }
+            )
+          </script>
         <?php endif; ?>
       </div>
-      <?php $recipientsQuery = new WP_Query(
-        array(
-          'post_type' => 'recipient',
-          'posts_per_page' => '-1',
-        )
-      );
-      if ($recipientsQuery->have_posts()):
-        ?>
-        <div class="swiper recipients-slider">
-          <div class="swiper-wrapper">
-            <?php while ($recipientsQuery->have_posts()):
-              $recipientsQuery->the_post(); ?>
-              <div class="swiper-slide">
-                <?php if (get_field('photo')):
-                  $photo = get_field('photo') ?>
-                  <img src="<?php echo esc_url($photo['sizes']['recipients-slider']) ?>" alt="<?php the_title() ?>">
-                <?php endif ?>
-                <div class="slide-content">
-                  <h3><?php the_title(); ?></h3>
-                  <?php if (get_field('school')): ?>
-                    <p class="school">
-                      <?php echo esc_attr(get_field('school')) ?>
-                    </p>
-                  <?php endif;
-                  if (get_field('bio')): ?>
-                    <p>
-                      <?php echo esc_attr(get_field('bio')) ?>
-                    </p>
-                  <?php endif; ?>
-                </div>
-              </div>
-            <?php endwhile;
-            wp_reset_postdata(); ?>
-          </div>
-          <div class="swiper-pagination"></div>
-          <button class="swiper-button-prev"></button><button class="swiper-button-next"></button>
-        </div>
-        <script>
-          const recipientsSlider = new Swiper(
-            '.swiper.recipients-slider',
-            {
-              loop: true,
-              navigation: {
-                nextEl: '.recipients-slider .swiper-button-next',
-                prevEl: '.recipients-slider .swiper-button-prev',
-              },
-              pagination: {
-                el: '.recipients-slider .swiper-pagination'
-              }
-            }
-          )
-        </script>
-      <?php endif; ?>
     </section>
 
   <?php endif;
