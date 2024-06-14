4adbcbf6c13305eb42a0cbcecc253c5230e33fd3
Ben Reiner <ben@mcguinnessmedia.com>
2024-06-14 12:30:56 -0400

    Add donation page

--- wp-content/themes/mmm/page-donate.php
+++ wp-content/themes/mmm/page-donate.php
@@ -1,5 +1,19 @@
 <?php get_header(); ?>
 
-<h1>Hello world!</h1>
+<section class="donation-form">
+  <div class="container">
+    <img src="<?php echo get_template_directory_uri() . '/assets/images/primary-logo.png' ?>"
+      alt="The Sarah L. Gamble Foundation">
+
+    <h1>Make a Tax-Deductible Donation</h1>
+
+    <hr>
+
+    <p>Please complete the form below to submit your donation.<br>
+      All donations are tax-deductible.</p>
+
+    <?php echo do_shortcode('[gravityform id="3" title="false"]'); ?>
+  </div>
+</section>
 
 <?php get_footer(); ?>
