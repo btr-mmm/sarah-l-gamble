20f61f9a62636aac66b4b23c923f6601036b8379
Ben Reiner <ben@mcguinnessmedia.com>
2024-06-17 16:05:39 -0400

    Add application page

---
+++ wp-content/themes/mmm/page-application.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<section class="application-form">
+  <div class="container">
+    <?php echo do_shortcode('[contact-form-7 id="8ff4ab6"]') ?>
+  </div>
+</section>
+
+<?php get_footer(); ?>
