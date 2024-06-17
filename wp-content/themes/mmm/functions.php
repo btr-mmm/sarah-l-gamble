4e934fd1dc5fc886f7790869487866bf616bf984
Ben Reiner <ben@mcguinnessmedia.com>
2024-06-17 16:22:22 -0400

    Add application form styles

--- wp-content/themes/mmm/functions.php
+++ wp-content/themes/mmm/functions.php
@@ -12,6 +12,7 @@ function slgEnqueueFiles()
   wp_enqueue_style('slg-donate', get_template_directory_uri() . '/css/donate.css', ['slg-main'], rand(111, 9999));
   wp_enqueue_style('slg-scholarships', get_template_directory_uri() . '/css/scholarships.css', ['slg-main'], rand(111, 9999));
   wp_enqueue_style('slg-contact-page', get_template_directory_uri() . '/css/contact-page.css', ['slg-main'], rand(111, 9999));
+  wp_enqueue_style('slg-application', get_template_directory_uri() . '/css/application.css', ['slg-main'], rand(111, 9999));
   wp_enqueue_style('slg-call-to-action', get_template_directory_uri() . '/css/call-to-action.css', ['slg-main'], rand(111, 9999));
 
   wp_enqueue_script('slg-responsive-nav', get_template_directory_uri() . '/js/responsive-nav.js');
@@ -30,3 +31,6 @@ function slgThemeSupport()
   add_image_size('cta', 960, 640, true);
   add_image_size('recipients-slider', 500, 700, true);
 }
+
+// Remove auto paragraph tags from CF7
+add_filter('wpcf7_autop_or_not', '__return_false');
